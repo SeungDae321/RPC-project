65f0c7518656239ecce9ba79302766a9bb4ccd8a
Seungdae321 <seungdae33@gmail.com>
2023-05-27 00:02:34 +0900

    added new goal and changed rate system

--- index.js
+++ index.js
@@ -13,19 +13,26 @@ const winStatic = document.querySelector('.winStatic');//승리통계창
 const loseStatic = document.querySelector('.loseStatic');//패배통계창
 const tieStatic = document.querySelector('.tieStatic');//무승부통계창
 const rates = document.querySelector('.rates');//승률통계창
+//n VS n 선택자
+const situation = document.querySelector('.situation');
+
 rates.addEventListener('click',()=>{
-    alert('무승부는 승리의 0.5로 취급한다.')
+    alert('무승부는 승리로 취급하지 않는다.')
 })
 
+ 
 //게임통계
 let winRate = 0;
 let loseRate = 0;
 let tieRate = 0;
 let totalGame = () => winRate + loseRate + tieRate;
 const getTotalRate = () =>{
-    let rate = winRate+(tieRate*0.5)/(totalGame())*100;
+    let rate = winRate/totalGame()*100;
     return rate.toFixed(3);
 }
+
+
+
 
 //승패함수
 const win = ()=>{
@@ -82,6 +89,20 @@ const clickEvt = (playerValue) =>{
     loseStatic.innerHTML = `패배: ${loseRate}`;
     tieStatic.innerHTML = `무승부: ${tieRate}`;
     rates.innerHTML = `승률: ${getTotalRate()}%`
+    situation.innerHTML = `${winRate} VS ${loseRate}`
+
+    //최종게임 결과
+    if(winRate >= 5){
+        alert('승리!!!')
+        restart();
+    } else if(loseRate >= 5){
+        alert('패배...ㅜㅠ')
+        restart()
+    }
+    /**
+     * 게임함수 안에서만 winRate나 loseRate가 바뀌니까
+     * Rate에 의존하는 최종게임 결과 로직은 게임함수 안에서 동작해야 한다.
+     */
 }
 
 //가위를 눌렀을 때
@@ -98,7 +119,18 @@ paperBtn.addEventListener('click',()=>{
 })
 
 //초기화를 눌렀을 때
-clearBtn.addEventListener('click',()=>{
+
+clearBtn.addEventListener('click',(restart))
+
+
+//통계를 눌렀을 때
+onOffStatics.addEventListener('click',()=>{
+    allStatics.classList.toggle("offStatic")
+})
+
+
+//재시작 함수 - 엥커링이 되도록 function키워드로 선언함.
+function restart(){
     winRate = 0;
     loseRate = 0;
     tieRate = 0;
@@ -109,10 +141,6 @@ clearBtn.addEventListener('click',()=>{
     winStatic.innerHTML = `승리: ${winRate}`;
     loseStatic.innerHTML = `패배: ${loseRate}`;
     tieStatic.innerHTML = `무승부: ${tieRate}`;
+    situation.innerHTML = 'player VS AI'
     console.clear()
-})
-
-//통계를 눌렀을 때
-onOffStatics.addEventListener('click',()=>{
-    allStatics.classList.toggle("offStatic")
-})
+}
